516d4f1863b62e5e5a0a120bf231164564e8dbda
Aniket Walanj <ixaaniketwalanj@gmail.com>
2026-07-30 00:18:57 +0530

    chore: fix nested git repo and untrack submodule of Codessiance-26

---
+++ Codessiance-26/codessiance-26/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    '192.168.1.37',
+    'alessandro-necessitous-leandro.ngrok-free.dev'
+  ],
+};
+
+export default nextConfig;
